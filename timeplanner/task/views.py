8f73385a47312a87c8da5e09f8b93295a062e9a5
andytwoods <andytwoods@gmail.com>
2019-05-30 20:17:19 +0100

    ajax start

--- timeplanner/task/views.py
+++ timeplanner/task/views.py
@@ -1,10 +1,20 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 from .helpers import expand_tasks
 from .models import Task
 
 # Create your views here.
+def run_command(command):
+    print(command)
+    return JsonResponse({})
+
+
 def home(request):
+    
+    command = request.GET.get('command', None)
+    if command is not None:
+        return run_command(command)
 
     tasks = Task.ordered_tasks()
     expanded_tasks = expand_tasks(tasks)
